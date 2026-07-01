215bae2422c32ee69dba55108a7383e5b8f0dd18
Parkman145 <parkerlearmonth@gmail.com>
2026-07-01 01:41:08 -0700

    Setup Config

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import requests
 import auth
 
 token = auth.get_token()
+
+# Setup Config
+with open("config.json") as f:
+    config = json.load(f) 
+
+exclude_other_users = config["exclude"]["exclude_other_users"]
+exclude_playlists = config["exclude"]["playlists"]
+exclude_songs = config["exclude"]["songs"]
+
